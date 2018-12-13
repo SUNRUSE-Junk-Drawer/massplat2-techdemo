7752a53b77d8199ec13273bc439e5bbaa852348a
jameswilddev <jameswilddev@outlook.com>
2018-12-13 20:16:25 +0000

    Added a type for textures.

--- index.ts
+++ index.ts
@@ -4,5 +4,6 @@
 /// <reference path="vectors.ts" />
 /// <reference path="gl-constants.ts" />
 /// <reference path="gl.ts" />
+/// <reference path="gl-texture.ts" />
 ///// <reference path="gl-chaos-monkey.ts" />
 /// <reference path="event-loop.ts" />
